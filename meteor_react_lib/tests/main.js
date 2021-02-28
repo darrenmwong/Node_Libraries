582096892abb5bed6c1610b553ba16e92397b7d9
Darren Wong <darrenwong@darrens-mbp.lan>
2021-02-27 18:29:40 -0800

    sweet jesus metoer cleanup

--- meteor_react_lib/tests/main.js
+++ meteor_react_lib/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("simple_todos_react", function () {
+describe("meteor_react_lib", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "simple_todos_react");
+    assert.strictEqual(name, "meteor_react_lib");
   });
 
   if (Meteor.isClient) {
